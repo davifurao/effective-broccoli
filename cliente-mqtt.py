8eba9e00996dcb2e6f2c93303daa90b1e6579647
Davi Luna <Sdavi738@gmail.com>
2022-11-29 01:31:51 -0300

    Início da construção do cliente em paho e o verificador em bash

---
+++ cliente-mqtt.py
@@ -0,0 +1,6 @@
+#obviamente precisa do python instalado, na versão 3
+#Precisa ser instalado o pip no pc para instalar o paho
+
+import paho.mqtt.client as mqtt #importando a biblioteca do  paho
+
+
