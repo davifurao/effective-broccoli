6755337294f21aba4e5860829f1629e6d99b8755
Davi Luna <Sdavi738@gmail.com>
2022-11-30 07:01:02 -0300

    Código simples para modificar depois
    Ainda aprendendo os métodos do paho. Falta testar com multiplos subscribes(para diferenciar a mensagem)

--- cliente-mqtt.py
+++ cliente-mqtt.py
@@ -3,4 +3,17 @@
 
 import paho.mqtt.client as mqtt #importando a biblioteca do  paho
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("$SYS/#")
 
+    def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("mqtt.eclipseprojects.io", 1883, 60)#broker,port,keepalive
+#creio que o bind address será adicionado num server
+
+client.loop_forever()
