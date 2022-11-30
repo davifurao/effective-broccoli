3d8e314ff3b9e019255ae436471c771e9e4ad764
Davi Luna <Sdavi738@gmail.com>
2022-11-30 07:08:09 -0300

    Comentários sobre o bash
    Comentários sobre o bash de verificação e execução do subscriber.

--- inicializador-do-programa.bash
+++ inicializador-do-programa.bash
@@ -13,3 +13,16 @@ fi
 
 
 #O bash está com erro de sintaxe, mas o objetivo é verificar se o python3, paho e mosquitto estão instalados. E caso contrário, instalá-los
+
+
+#deverá ser construído da seguinte forma: 
+#verificar se o python3 está instalado
+#verificar se o pip está instalado
+#tornar o subscriber executável(com chmod)
+#verificar se o selinux está ativado, para verificar se o sistema consegue executar o paho
+#iniciar o subscriber em paho(python), iniciando o arquivo da pasta(caminho)
+
+
+
+
+#OBS: Caso dê algum erro, poderei pedir para o sistema dar um git clone do repositório(claro, verificando se o git está instalado anteriormente, talvez por meio do apt)
