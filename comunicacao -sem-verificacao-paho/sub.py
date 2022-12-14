7bfe9830650fe38cb9a0f7bb18bc1642f11f5f3b
Davi Luna <Sdavi738@gmail.com>
2022-12-14 02:45:03 -0300

    Create the funcionality of write in txt archive

--- comunicacao -sem-verificacao-paho/sub.py
+++ comunicacao -sem-verificacao-paho/sub.py
@@ -6,8 +6,10 @@ HOST= "test.mosquitto.org"
 PORT=1883
 keepalive=60
 bind_address=""
-TOPIC=[("teste",0)]#tupla com tópico e QoS. Pode-se adicionar diversos tópicos 
-#TOPIC= "teste"
+TOPIC=[("teste",0),("teste2",0)]#tupla com tópico e QoS. Pode-se adicionar diversos tópicos 
+NAME_archive=TOPIC#aqui que irá definir o nome do arquivo(por padrão é o nome do tópico, junto com o QOS)
+
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -15,14 +17,23 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(TOPIC)
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print("=============================")
+    print("Topic: "+msg.topic)
+    print("Payload: "+str(msg.payload))
+    print("=============================")
+
+    for i in range(len(TOPIC)):
+        with open(f'{TOPIC[i]}.txt','at') as f:#instanciou a variavel f para abrir/criar o arquivo com o nome do tópico(ou qualquer outro que tenha alterado)
+            f.write(f'{str(msg.payload)}\n') 
+            f.close()
+
+    
 
 
-
+message='teste'
 client = mqtt.Client("python3")
 client.on_connect = on_connect
 client.on_message = on_message
-
 
 client.connect(HOST, PORT, keepalive,bind_address)
 
