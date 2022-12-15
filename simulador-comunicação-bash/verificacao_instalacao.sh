e4aa16c23f8db1caac8f780d6ce194897e87b116
Davi Luna <86129094+davifurao@users.noreply.github.com>
2022-12-15 15:14:45 -0300

    adding the paho instalation command

--- simulador-comunicação-bash/verificacao_instalacao.sh
+++ simulador-comunicação-bash/verificacao_instalacao.sh
@@ -7,6 +7,7 @@ sudo apt install mosquitto
 sudo apt install mosquitto-clients
 sudo apt-get install python3
 sudo apt-get install python3-pip
+pip install paho-mqtt
 sudo apt-get update
 sudo apt-get upgrade
 sudo service mosquitto start
