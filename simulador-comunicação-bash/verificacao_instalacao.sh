fddebf96ab407ef7d2a32c666471719792e0acd2
Davi Luna <86129094+davifurao@users.noreply.github.com>
2022-12-15 14:50:34 -0300

    Create a archive bash with a beta-verification

---
+++ simulador-comunicação-bash/verificacao_instalacao.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+saida_instrucao = "Caso você queira terminar a operação, aperte Ctrl+c"
+function atualizacao(){
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install mosquitto
+sudo apt install mosquitto-clients
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get update
+sudo apt-get upgrade
+sudo service mosquitto start
+
+}
+
+atualizacao
